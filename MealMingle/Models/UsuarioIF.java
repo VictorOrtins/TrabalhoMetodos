d994d71776d0a18efdd8e1c478c0f74c18aaa316
Carol Aguiar <acrla@academico.ufpb.br>
2024-03-02 23:38:53 -0300

    Ajuste UsuarioIF.java

--- MealMingle/Models/UsuarioIF.java
+++ MealMingle/Models/UsuarioIF.java
@@ -1,21 +1,7 @@
 package MealMingle.Models;
 
 public interface UsuarioIF {
-    // Métodos que todos os usuários devem implementar
-    void adicionar();
-    void atualizar();
-    void remover();
+
+    
+
 }
-
-public abstract class UsuarioAbstrato implements UsuarioIF {
-    // Campos comuns a todos os usuários
-    protected String id;
-    protected String nome;
-
-    // Implementação de métodos comuns, se houver
-    // Os métodos abstratos serão implementados pelas subclasses
-    public abstract void adicionar();
-    public abstract void atualizar();
-    public abstract void remover();
-}
-
